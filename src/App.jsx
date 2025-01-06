0382219f529dac93d47c23131542c57f5bc8d9ae
George Konstantinou <george.krl95@gmail.com>
2025-01-06 03:00:07 +0200

    fixed again

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,10 @@ import Technologies from './components/Technologies'
 import MyExperience from './components/MyExperience'
 import Projects from './components/Projects'
 import ContactMe from './components/ContactMe'
-import SeoImage from './components/seoImage'
 
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900">
-      <SeoImage />
       <div className="fixed top-0 -z-10 h-full w-full">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
