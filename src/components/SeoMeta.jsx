40464e431022784b722aaf950394cd2350762cee
George Konstantinou <george.krl95@gmail.com>
2025-01-06 23:59:02 +0200

    meta description

--- src/components/SeoMeta.jsx
+++ src/components/SeoMeta.jsx
@@ -10,7 +10,7 @@ function SeoMeta() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://gikonst.github.io/george-portfolio"/>
       <meta property="og:title" content="George's Portfolio Website"/>
-      <meta property="og:description" content="Explore George's projects, skills, and portfolio."/>
+      <meta property="og:description" content="Explore my work and projects in software development and programming. Learn more about my experience and skills."/>
     </Helmet>
     </HelmetProvider>
   );
