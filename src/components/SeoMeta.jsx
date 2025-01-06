017ea5f7d07b2c40d0286e3ac6baadebbbcf7d70
George Konstantinou <george.krl95@gmail.com>
2025-01-06 22:56:06 +0200

    meta

--- src/components/SeoMeta.jsx
+++ src/components/SeoMeta.jsx
@@ -8,6 +8,9 @@ function SeoMeta() {
       <meta property="og:image" content="https://gikonst.github.io/george-portfolio/preview.png" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://gikonst.github.io/george-portfolio" />
+      <meta property="og:image:width" content="1200"/>
+      <meta property="og:image:height" content="630"/>
+      
     </Helmet>
     </HelmetProvider>
   );
