33aa9c1195e8313b22bd203b7bca5609dc161434
George Konstantinou <george.krl95@gmail.com>
2025-01-06 22:36:39 +0200

    seoMeta

---
+++ src/components/SeoMeta.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
+function SeoMeta() {
+  return (
+    <HelmetProvider>
+    <Helmet>
+      <meta property="og:image" content="https://gikonst.github.io/george-portfolio/preview.jpg" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gikonst.github.io/george-portfolio" />
+    </Helmet>
+    </HelmetProvider>
+  );
+}
+
+export default SeoMeta;
